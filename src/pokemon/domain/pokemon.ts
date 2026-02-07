53b33d180a57a4338f0a41e94014a8aa10586f59
Mendez <yahirelian.mendez@outlook.com>
2026-02-07 15:44:26 +0100

    refactor(domain): make pokemon the main entity. include stats, skills, and locations directly in pokemon

--- src/pokemon/domain/pokemon.ts
+++ src/pokemon/domain/pokemon.ts
@@ -1,7 +1,14 @@
+import { Location } from './location';
+import { Skill } from './skill';
+import { Stat } from './stat';
+
 export interface Pokemon{
     id:number,
     name:string,
     frontImage:string,
     animatedImage:string,
     baseExperience:number,
+    skills:Skill[],
+    stats:Stat[],
+    locations:Location[],
 }
